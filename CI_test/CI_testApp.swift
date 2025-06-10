cbc3e2975565d208596a1cf98060d82a47c26625
YevheniiLevchenko <yevhenii@sportsbox.ai>
2025-06-10 16:37:11 +0300

    first commit

---
+++ CI_test/CI_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  CI_testApp.swift
+//  CI_test
+//
+//  Created by Yevhenii Levchenko on 10.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct CI_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CI_test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CI_test
+//
+//  Created by Yevhenii Levchenko on 10.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
